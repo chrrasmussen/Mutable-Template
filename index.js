b272c594144afb961b92d178607724903bcbe0df
Christian Rasmussen <christian.rasmussen@me.com>
2015-08-27 00:18:19 +0200

    Enabled support for ES6.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 #!/usr/bin/env node
 
-// SOURCE: http://javascriptplayground.com/blog/2015/03/node-command-line-tool/
-
-var userArgs = process.argv.slice(2);
-var searchPattern = userArgs[0];
-
+var fs = require('fs');
 var exec = require('child_process').exec;
-var child = exec('ls -a | grep ' + searchPattern, function(err, stdout, stderr) {
-  console.log(stdout);
+
+
+var distFile = 'dist/index.js';
+var sourceFile = 'src/index.js';
+var nodeExec = 'node';
+var babelExec = './node_modules/.bin/babel-node';
+
+
+var distExists = false;
+try {
+    var stats = fs.lstatSync(distFile);
+    if (stats.isFile()) {
+        distExists = true;
+    }
+}
+catch (e) {
+    // Silence error
+}
+
+var args;
+if (distExists) {
+    args = [nodeExec, distFile];
+}
+else {
+    args = [babelExec, sourceFile];
+}
+
+var command = args.join(' ');
+var child = exec(command, function(err, stdout, stderr) {
+    if (!err) {
+        console.log(stdout);
+    }
+    else {
+        console.log(stderr);
+    }
 });
